e582f5b055af003582849a263a74f4126610bd53
Prof. Deepak Mishra <deepakmishra2698@gmail.com>
2026-04-02 10:51:02 +0530

    js

---
+++ javascript/variable.js
@@ -0,0 +1,45 @@
+
+// var num = 10;
+
+// console.log(num);
+// console.log(typeof num);
+
+// var num = '10';
+
+// console.log(num);
+// console.log(typeof num);
+
+// var num = true;
+
+// console.log(num);
+// console.log(typeof num);
+
+// var num = 10.67;
+
+// console.log(num);
+// console.log(typeof num);
+
+// var domain = 'Java';
+
+// console.log(domain);
+// console.log(typeof domain);
+
+// var domain = 'S';
+
+// console.log(domain);
+// console.log(typeof domain);
+
+// var nully = null;
+
+// console.log(nully);
+// console.log(typeof nully);
+
+// var undef;
+
+// console.log(undef);
+// console.log(typeof undef);
+
+// undef = 'Now I have a value';
+
+// console.log(undef);
+// console.log(typeof undef);
